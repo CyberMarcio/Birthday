f67a00a9c53103297cfd5356d8d7d76c604ee2b0
marcio <viana_marcio@outlook.com>
2022-04-10 16:22:27 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val currentYear: Int = 2022
+    val birthYear = 1979
+    val age = currentYear - birthYear
+
+    println("At the end of $currentYear you will be $age years old.")
+}
